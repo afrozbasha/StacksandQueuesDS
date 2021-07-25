4b4c799783f0f273beafe6d0a2e5476d5426838c
afrozbasha <iafrozbasha@gmail.com>
2021-07-25 13:23:12 +0530

    Ability to create a Stack of 56->30->70 and Peak and PoP from the Stack till it is empty

--- Stack.java
+++ Stack.java
@@ -43,6 +43,25 @@ public class Stack {
         }
     }
 
+    //Method to display the stack Peek value
+    public void peek(){
+        if (top == null)
+            System.out.println("Stack is underflow");
+        else
+            System.out.println("Stack Peek value is : " + top.data);
+    }
+
+    //Method to display the stack Pop value
+    public void pop(){
+        if (top == null)
+            System.out.println("Stack is underflow");
+        else{
+            System.out.println("Stack Pooped value is : " + top.data);
+            top = top.next;
+        }
+
+    }
+
     public static void main(String[] args) {
         Stack stack = new Stack();
 
@@ -51,8 +70,16 @@ public class Stack {
         stack.push(30);
         stack.push(56);
 
+        
         //Method to display stack in First In Last Out Order
         stack.display();
 
+        //Peak and PoP from the Stack till it is empty
+        while (stack.top != null){
+            stack.peek();
+            stack.pop();
+        }
+        if (stack.top == null)
+            System.out.println("Stack is empty");
     }
 }
