168e8726373471374cc8cc9073580052f3d777f3
afrozbasha <iafrozbasha@gmail.com>
2021-07-25 15:24:55 +0530

    Ability to create a Queue of 56->30->70->40 and DeQueue till Empty

--- Queue.java
+++ Queue.java
@@ -34,12 +34,51 @@ public class Queue {
 
     /// Method to display Queue in First In First Out Order
     public void display() {
+        if (front == null)
+            System.out.println("Que is Empty");
         QNode temp = new QNode();
         temp = front;
         while (temp != null) {
             System.out.println(temp.data);
             temp = temp.next;
         }
+    }
+
+    //Method to push int element in the DeQue using front rear
+    public void deAllQue(){
+        if (front == null){
+            System.out.println("Queue is Underflow and Empty");
+            return;
+        }
+        while (front != null){
+            QNode temp = front;
+            System.out.println("Successfully DnQue the data : " + front.data);
+            front = front.next;
+        }
+        rear = null;
+    }
+
+    //Method to find size
+    public void deQue(){
+        if (front == null){
+            System.out.println("Queue is Underflow and Empty");
+            return;
+        }
+        System.out.println("Successfully DnQue the data : " + front.data);
+        front = front.next;
+    }
+
+    public void size(){
+        if (front == null){
+            System.out.println("Que is Empty Size is Zero");
+            return;
+        }
+        int i=0;
+        while (front != null) {
+            i++;
+            front = front.next;
+        }
+        System.out.println("Que is Size is " + i);
     }
 
 
@@ -50,13 +89,25 @@ public class Queue {
         Que.enQue(56);
         Que.enQue(30);
         Que.enQue(70);
+        Que.enQue(40);
+        Que.display();
+
+
+        //Method to remove int element in the Queue using front rear
+        Que.deQue();
 
         /// Method to display Queue in First In First Out Order
         Que.display();
 
+        //Method to remove all int element in the Queue using front rear
+        Que.deAllQue();
+
+        Que.display();
+
+        //Method to find size
+        Que.size();
+
 
     }
-
-
 
 }
